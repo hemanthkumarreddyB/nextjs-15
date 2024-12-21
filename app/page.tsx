3cace29a59bc06c62da42f68a8f022f0c2661540
hemanthkumarreddyB <48670482+hemanthkumarreddyB@users.noreply.github.com>
2024-12-21 14:35:14 +0530

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ const Home = () => {
       <h1 className="background-light850_dark100 h1-bold">
         welcome to the world of next.js
       </h1>
+      <h1 className="background-light850_dark100 h1-bold font-space-grotesk">
+        welcome to the world of next.js
+      </h1>
     </>
   );
 };
